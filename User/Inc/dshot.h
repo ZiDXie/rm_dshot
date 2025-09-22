82ae82dd2e5f37ac370e5aec77c30595a97a94dd
ZiDXie <shengxie47@gmail.com>
2025-09-22 21:59:16 +0800

    Add Dshot packet preparation function and DMA buffer definitions.

--- User/Inc/dshot.h
+++ User/Inc/dshot.h
@@ -5,6 +5,10 @@
 #ifndef RM_DSHOT_DSHOT_H
 #define RM_DSHOT_DSHOT_H
 
+//User include
+#include "main.h"
+#include "stdbool.h"
+
 ///For dshot600 on 168MHz F407
 ///The term of pwm is 1.67us,the bit1 is 1.25us high level, and the bit0 is 0.625us high level
 #define MOTOR_BIT_0
@@ -15,5 +19,8 @@
 ///but since the TIM BURST DMA method requires resetting at the end of the frame to prevent continuous signal output,
 ///two additional bits are added at the end with actual compare values of 0.
 #define DSHOT_DMA_BUFFER_SIZE   18
+
+uint32_t motor1_dmaBurstBuffer[DSHOT_DMA_BUFFER_SIZE];
+uint32_t motor2_dmaBurstBuffer[DSHOT_DMA_BUFFER_SIZE];
 
 #endif //RM_DSHOT_DSHOT_H
--- User/Src/dshot.c
+++ User/Src/dshot.c
@@ -3,3 +3,29 @@
 //
 
 #include "dshot.h"
+
+/// @brief To check CRC and prepare the dshot packet
+/// @param value
+/// The throttle value ranges from 0 to 2047, with values from 48 to 2047 representing throttle levels from 0% to 100%.
+/// Values from 0 to 47 are reserved for special commands.
+/// @param requestTelemetry
+/// Whether to request the return of data
+/// @return Dshot packet
+static uint16_t prepareDshotPacket(const uint16_t value, bool requestTelemetry)
+{
+    // throttle is 11 bits, so shift left 1 bit and add telemetry request bit to make 12 bits
+    uint16_t packet = (value << 1) | (requestTelemetry ? 1 : 0);
+    // checksum is 4 bits, so we need to shift left 4 bits and add it to make 16 bits
+    // compute checksum
+    int csum = 0;
+    int csum_data = packet;
+    for (int i = 0; i < 3; i++) {
+        csum ^=  csum_data;   // xor data by nibbles
+        csum_data >>= 4;
+    }
+    // Protect lower 4 bits
+    csum &= 0xf;
+    // append checksum
+    packet = (packet << 4) | csum;
+    return packet;
+}

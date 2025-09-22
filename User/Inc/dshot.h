3c4470859cce05862c53785af70198ce5730067c
ZiDXie <shengxie47@gmail.com>
2025-09-22 21:37:27 +0800

    Init project.

---
+++ User/Inc/dshot.h
@@ -0,0 +1,19 @@
+//
+// Created by xie on 2025/9/22.
+//
+
+#ifndef RM_DSHOT_DSHOT_H
+#define RM_DSHOT_DSHOT_H
+
+///For dshot600 on 168MHz F407
+///The term of pwm is 1.67us,the bit1 is 1.25us high level, and the bit0 is 0.625us high level
+#define MOTOR_BIT_0
+#define MOTOR_BIT_1
+#define MOTOR_BITLENGTH
+
+///Normally, a DSHOT frame contains only 16 bits of data,
+///but since the TIM BURST DMA method requires resetting at the end of the frame to prevent continuous signal output,
+///two additional bits are added at the end with actual compare values of 0.
+#define DSHOT_DMA_BUFFER_SIZE   18
+
+#endif //RM_DSHOT_DSHOT_H
---
+++ User/Src/dshot.c
@@ -0,0 +1,5 @@
+//
+// Created by xie on 2025/9/22.
+//
+
+#include "dshot.h"
